67d8688b118aae464f48b1905aee495f62a60aa9
Shalaka <shalakaparate383@gmail.com>
2020-09-22 22:17:07 +0530

    first commit

---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	public class SeleniumTest {
+
+		@Test
+		public void PostJira()
+		{
+			System.out.println("PostJira");
+		}
+		@Test
+		public void TwitterDefect()
+	{
+			System.out.println("TwitterDefect");
+	}
+}
+}
